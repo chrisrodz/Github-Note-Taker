b647dad43422c2705bb3f781e0f59450e2957008
Christian <christian.etpr10@gmail.com>
2017-03-10 08:58:37 -0800

    Add persisten storage through firebase

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,9 +3,12 @@ var Router = require('react-router');
 var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 var Profile = React.createClass({
-  getInitialState: function() {
+  mixins: [ReactFireMixin],
+  getInitialState() {
      return {
       notes: [1, 2, 3],
       bio: {
@@ -14,17 +17,25 @@ var Profile = React.createClass({
       repos: ['a', 'b', 'c']
      }
   },
-  render: function() {
+  componentDidMount() {
+    this.ref = new Firebase('https://github-note-taker-ef11e.firebaseio.com/');
+    var childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnMount() {
+    this.unbind('notes');
+  },
+  render() {
     return (
       <div className="row">
         <div className="col-md-4">
           <UserProfile username={this.props.params.username} bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          <Repos repos={this.state.repos}/>
+          <Repos username={this.props.params.username} repos={this.state.repos}/>
         </div>
         <div className="col-md-4">
-          <Notes notes={this.state.notes}/>
+          <Notes username={this.props.params.username} notes={this.state.notes}/>
         </div>
       </div>
     )
