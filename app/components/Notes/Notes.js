594f3cad2908d6fb48be6a75581c131cc896c719
Christian <christian.etpr10@gmail.com>
2017-03-12 10:53:19 -0700

    Refactor notes to es6

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,14 +1,26 @@
-var React = require('react');
-var NotesList = require('./NotesList');
-var AddNote = require('./AddNote');
+import React from 'react';
+import NotesList from './NotesList';
+import AddNote from './AddNote';
 
-var Notes = React.createClass({
-  propTypes: {
-    username: React.PropTypes.string.isRequired,
-    notes: React.PropTypes.array.isRequired,
-    addNote: React.PropTypes.func.isRequired
-  },
-  render: function() {
+// var Notes = React.createClass({
+//   propTypes: {
+//     username: React.PropTypes.string.isRequired,
+//     notes: React.PropTypes.array.isRequired,
+//     addNote: React.PropTypes.func.isRequired
+//   },
+//   render: function() {
+//     return (
+//       <div> 
+//         <h3> Notes for {this.props.username} </h3>
+//         <AddNote username={this.props.username} addNote={this.props.addNote} />
+//         <NotesList notes={this.props.notes} />
+//       </div>
+//     )
+//   }
+// });
+
+class Notes extends React.Component {
+  render() {
     return (
       <div> 
         <h3> Notes for {this.props.username} </h3>
@@ -17,6 +29,12 @@ var Notes = React.createClass({
       </div>
     )
   }
-});
+}
 
-module.exports = Notes;
+Notes.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  notes: React.PropTypes.array.isRequired,
+  addNote: React.PropTypes.func.isRequired
+}
+
+export default Notes;
